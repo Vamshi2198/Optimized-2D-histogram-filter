7d8c4ef4045470d9f81d40ba38c9160344e920e6
Vamshi_Krishna <vamshikrishna.t21@gmail.com>
2021-07-03 19:11:28 -0400

    Optimized normalize.cpp -> (reference, extra variables)

--- normalize.cpp
+++ normalize.cpp
@@ -1,36 +1,25 @@
 #include "headers/normalize.h"
 using namespace std;
 
-vector< vector<float> > normalize(vector< vector <float> > grid) {
+// OPTIMIZATION: Pass variable by reference
+vector< vector<float> > normalize(vector< vector <float> > &grid) {
 
+  	// OPTIMIZATION: Avoid declaring and defing intermediate variables that are not needed.
 	float total = 0.0;
-	int i;
-	int j;
-	vector<float> row;
-	vector<float> newRow;
-	float oldProb;
+	int i,j;
 	for (i = 0; i < grid.size(); i++)
 	{
-		row = grid[i];
-		for (j=0; j< row.size(); j++)
+		for (j=0; j< grid[0].size(); j++)
 		{
-			oldProb = row[j];
-			total += oldProb;
+			total += grid[i][j];
 		}
 	}
-
-	vector< vector<float> > newGrid;
 
 	for (i = 0; i < grid.size(); i++) {
-		vector<float> row = grid[i];
-		newRow.clear();
-		for (j=0; j< row.size(); j++) {
-			float oldProb = row[j];
-			float newProb = oldProb / total;
-			newRow.push_back(newProb);
+		for (j=0; j< grid[0].size(); j++) {
+			grid[i][j] = grid[i][j] / total;
 		}
-		newGrid.push_back(newRow);
 	}
 
-	return newGrid;
+	return grid; 
 }
