f8281c424ef0b3f4cfa0888a3394db286e3d2361
Vamshi_Krishna <vamshikrishna.t21@gmail.com>
2021-07-03 18:56:14 -0400

    Optimized blur.cpp -> (for loop)

--- blur.cpp
+++ blur.cpp
@@ -4,89 +4,26 @@
 using namespace std;
 
 vector < vector <float> > blur(vector < vector < float> > grid, float blurring) {
-
-  	vector < vector <float> > window;
-	vector < vector <float> > newGrid;
-	vector <float> row;
-	vector <float> newRow;
-
-	int height = grid.size();
-	int width = grid[0].size();
 	
 // OPTIMIZATION: Used static keyword function
     static float center = 1.0 - blurring;
 	static float corner = blurring / 12.0;
 	static float adjacent = blurring / 6.0;
 
+	static vector < vector <float> > window = { {corner, adjacent, corner}, {adjacent, center, adjacent}, {corner, adjacent, corner} };
 
-	int i, j;
+	static vector <int> DX = {-1, 0, 1};
+	static vector <int> DY = {-1, 0, 1};
+
+	
+	vector < vector <float> > newGrid;
+	vector <float> row;
+	vector <float> newRow;
+
+	int height = grid.size();
+	int width = grid[0].size();
 	float val;
-
-	for (i=0; i<3; i++) {
-		row.clear();
-		for (j=0; j<3; j++) {
-			switch (i) {
-				case 0: 
-				switch (j) {
-					case 0: 
-					val = corner;
-					break;
-
-					case 1: 
-					val = adjacent;
-					break;
-
-					case 2: 
-					val = corner;
-					break;
-				}
-				break; 
-
-				case 1:
-				switch (j) {
-					case 0: 
-					val = adjacent;
-					break;
-
-					case 1: 
-					val = center;
-					break;
-					
-					case 2: 
-					val = adjacent;
-					break;
-				}
-				break;
-
-				case 2:
-				switch(j) {
-					case 0: 
-					val = corner;
-					break;
-
-					case 1: 
-					val = adjacent;
-					break;
-					
-					case 2: 
-					val = corner;
-					break;
-				}
-				break;
-			}
-			row.push_back(val);
-		}
-		window.push_back(row);
-	}
-
-
-	vector <int> DX;
-	vector <int> DY;
-
-	DX.push_back(-1); DX.push_back(0); DX.push_back(1);
-	DY.push_back(-1); DY.push_back(0); DY.push_back(1);
-
-	int dx, dy, ii, jj, new_i, new_j, multiplier, newVal;
+    int i, j, dx, dy, ii, jj, new_i, new_j, multiplier, newVal;
   
 	// OPTIMIZATION: Used improved zeros function
 	newGrid = zeros(height, width);
